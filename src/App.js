f06a6d7d8276b3392103c66dc201e2df895f46e5
Jonas Gabrielsson <jonaskgabrielsson@gmail.com>
2024-08-19 17:38:05 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import ammebil from './ammebil.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ammebil} className="App-logo" alt="logo" />
+        <p>
+          Välkommen till Amelia och Jonas bröllopssida! 
+          Mera information kommer att tillkomma inom kort!
+        </p>
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
